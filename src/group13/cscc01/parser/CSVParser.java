57a2a0bfc37abbe54020c706028d879c12e771d7
jnicol17 <jamienicol97@gmail.com>
2018-10-20 15:29:57 -0400

    Added info form and updated CSVParser

--- src/group13/cscc01/parser/CSVParser.java
+++ src/group13/cscc01/parser/CSVParser.java
@@ -1,6 +1,9 @@
-package group13.cscc01.main;
+package group13.cscc01.parser;
 
 import com.opencsv.*;
+
+import group13.cscc01.forms.InfoForm;
+
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
@@ -9,12 +12,18 @@ import java.util.HashMap;
 import java.util.LinkedHashMap;
 
 public class CSVParser {
-	private static final String SAMPLE_CSV_FILE_PATH = "/home/jamie/Desktop/iCARE_template.csv";
+	private static final String SAMPLE_CSV_FILE_PATH = "/home/jamie/Desktop/infoforum.csv";
     public static void main(String[] args) throws IOException {
         System.out.println("Hello CSV");
+        InfoForm record = new InfoForm();
+        System.out.println(record.getInfoMap());
+        for (String key : record.getInfoMap().keySet()){
+        	System.out.println(key);
+        }
         try (
                 Reader reader = Files.newBufferedReader(Paths.get(SAMPLE_CSV_FILE_PATH));
-        		CSVReader csvReader = new CSVReaderBuilder(reader).withSkipLines(5).build();
+        		//CSVReader csvReader = new CSVReaderBuilder(reader).withSkipLines(5).build();
+        		CSVReader csvReader = new CSVReader(reader);
             ) {
                 // Reading Records One by One in a String array
                 String[] nextRecord;
@@ -26,6 +35,11 @@ public class CSVParser {
                 	test.put(nextRecord[i], nextnextRecord[i]);
                 }
                 System.out.println(test);
+                //InfoForm record = new InfoForm();
+                //for (String key: test.keySet()){
+                //	record.updateInfoMap(key, test.get(key));
+                //}
+                //record.updateInfoMap()
         
         	}
 
