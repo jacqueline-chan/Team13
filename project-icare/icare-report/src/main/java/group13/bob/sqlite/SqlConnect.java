b4f4087f34dee83a08a1ae9ada19341282cddfbf
Ray Tong <raydude444@gmail.com>
2018-10-28 21:55:35 -0400

    added maven functionality to icare-report

--- project-icare/icare-report/src/main/java/group13/bob/sqlite/SqlConnect.java
+++ project-icare/icare-report/src/main/java/group13/bob/sqlite/SqlConnect.java
@@ -1,15 +1,19 @@
 package group13.bob.sqlite;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class SqlConnect {
 
-    private static final String DBLOCATION = "./project-icare/icare-client/src/group13/adam/db/test.db";
+    private static final String DBLOCATION = "../icare-db/test.db";
 
     public static Connection connect() {
         // SQLite connection string
+        Path currentDir = Paths.get(".");
+        System.out.println(currentDir.toAbsolutePath());
         String url = "jdbc:sqlite:" + DBLOCATION;
         Connection conn = null;
         try {
