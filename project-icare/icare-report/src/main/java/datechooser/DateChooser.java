d325bfcf20aa70dd1b98633d2a3d07ebb23ef8ae
AyanamiSean <suxin.hong@mail.utoronto.ca>
2018-11-06 21:01:46 -0500

    add a date chooser feature for bob so that he can select data from certain date range

---
+++ project-icare/icare-report/src/main/java/datechooser/DateChooser.java
@@ -0,0 +1,45 @@
+package datechooser;
+
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import com.toedter.calendar.JDateChooser;
+
+public class DateChooser extends JFrame{
+
+	private JFrame jf;
+	private JDateChooser dateChooser1;
+	private JDateChooser dateChooser2;
+
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					DateChooser datePicker = new DateChooser();
+					datePicker.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+
+	public DateChooser() {
+		
+		setBounds(500, 100, 450, 300);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		getContentPane().setLayout(null);
+		setTitle("Date Picker");
+		
+		dateChooser1 = new JDateChooser();
+		dateChooser1.setBounds(112, 24, 269, 44);
+		getContentPane().add(dateChooser1);
+		dateChooser1.setDateFormatString("yyyy-MM-dd");
+		
+		dateChooser2 = new JDateChooser();
+		dateChooser2.setBounds(112, 177, 269, 44);
+		getContentPane().add(dateChooser2);
+		dateChooser2.setDateFormatString("yyyy-MM-dd");
+	}
+}
