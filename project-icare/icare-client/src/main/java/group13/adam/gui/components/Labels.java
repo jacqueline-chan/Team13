28fe910f6b8079b0a7ba14d94d5336a6393796cf
Jacqueline Chan <jacquelinechan2@hotmail.com>
2018-11-07 03:24:11 -0500

    refactors the ApplicationForm by its components (strings, buttons, labels)

---
+++ project-icare/icare-client/src/main/java/group13/adam/gui/components/Labels.java
@@ -0,0 +1,58 @@
+package group13.adam.gui.components;
+
+import java.awt.Color;
+
+import javax.swing.JFormattedTextField;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class Labels {
+	
+	
+	public JLabel[] initializeJLabels(String[] fieldNames, JPanel labelPane, JFormattedTextField[] fields){
+		JLabel[] labels = new JLabel[100];
+		for (int i = 0; i < fieldNames.length; i++) {
+			  JLabel newLabel = new JLabel();
+			  labels[i] = newLabel;
+			  labels[i].setText(fieldNames[i]);
+			  labels[i].setLabelFor(fields[i]);
+			  labelPane.add(labels[i]);
+
+		}
+		return labels;	
+	}
+	
+	public JFormattedTextField[] initializeJFormattedTextField(String[] fieldNames, JPanel fieldPane ){
+		JFormattedTextField[] fields = new JFormattedTextField[100];
+	    for (int i = 0; i < fieldNames.length; i++) {
+	        JFormattedTextField newField = new JFormattedTextField();
+	    	fields[i] = newField;
+	        fields[i].setColumns(10);
+	        fieldPane.add(fields[i]);
+
+	    }
+	    
+	    
+	    
+	    return fields;
+		
+	}
+
+	
+	
+	
+	
+	
+	
+	public JLabel createErrorLabel(JPanel errorMessagePanel){
+	JLabel errorMessageLabel = new JLabel();
+    errorMessageLabel.setForeground(Color.red);
+    errorMessageLabel.setText(
+        "There was an error with your submission. Please review the highlighted fields.");
+    errorMessagePanel.add(errorMessageLabel);
+    errorMessagePanel.setVisible(false);
+	return errorMessageLabel;
+    
+	}
+
+}
