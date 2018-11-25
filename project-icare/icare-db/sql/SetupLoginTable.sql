315e28aef84a659c119b2ee3c9dc95efb54385dd
Jacqueline Chan <jacquelinechan2@hotmail.com>
2018-11-25 16:15:20 -0500

    Added instructions as to setting up login

--- project-icare/icare-db/sql/SetupLoginTable.sql
+++ project-icare/icare-db/sql/SetupLoginTable.sql
@@ -1,18 +1,18 @@
 DROP TABLE IF EXISTS Login;
 
-CREATE TABLE InfoForum(
+CREATE TABLE Login(
       username text PRIMARY KEY,
       password text NOT NULL,
       user_access text NOT NULL
 );
 
-insert into MOCK_DATA (username, password, user_access) values ('hceresa0', '19j03Wn8qInL', 'DEFAULT');
-insert into MOCK_DATA (username, password, user_access) values ('htellett1', 'Lc7mI0DYP', 'DEFAULT');
-insert into MOCK_DATA (username, password, user_access) values ('pbernardeschi2', 'mEWvmp', 'DEFAULT');
-insert into MOCK_DATA (username, password, user_access) values ('estandingford3', 'JCITIvIV8YK', 'DEFAULT');
-insert into MOCK_DATA (username, password, user_access) values ('oabriani4', 'lxtABz4sxK', 'DEFAULT');
-insert into MOCK_DATA (username, password, user_access) values ('bolander5', '5QFFSN0H0', 'DEFAULT');
-insert into MOCK_DATA (username, password, user_access) values ('rscritch6', 'wA0GqSJVC0B', 'DEFAULT');
-insert into MOCK_DATA (username, password, user_access) values ('hadan7', 'IEL2Ozuq1', 'DEFAULT');
-insert into MOCK_DATA (username, password, user_access) values ('mod', 'mod', 'MODERATOR');
-insert into MOCK_DATA (username, password, user_access) values ('admin', 'admin', 'ADMIN');
+insert into Login (username, password, user_access) values ('hceresa0', '19j03Wn8qInL', 'DEFAULT');
+insert into Login (username, password, user_access) values ('htellett1', 'Lc7mI0DYP', 'DEFAULT');
+insert into Login (username, password, user_access) values ('pbernardeschi2', 'mEWvmp', 'DEFAULT');
+insert into Login (username, password, user_access) values ('estandingford3', 'JCITIvIV8YK', 'DEFAULT');
+insert into Login (username, password, user_access) values ('oabriani4', 'lxtABz4sxK', 'DEFAULT');
+insert into Login (username, password, user_access) values ('bolander5', '5QFFSN0H0', 'DEFAULT');
+insert into Login (username, password, user_access) values ('rscritch6', 'wA0GqSJVC0B', 'DEFAULT');
+insert into Login (username, password, user_access) values ('hadan7', 'IEL2Ozuq1', 'DEFAULT');
+insert into Login (username, password, user_access) values ('mod', 'mod', 'MODERATOR');
+insert into Login (username, password, user_access) values ('admin', 'admin', 'ADMIN');
--- project-icare/icare-db/sql/SetupLoginTableBlank.sql
+++ project-icare/icare-db/sql/SetupLoginTableBlank.sql
@@ -1,6 +1,7 @@
 DROP TABLE IF EXISTS Login;
 
-CREATE TABLE InfoForum(
-      Username text PRIMARY KEY,
-      Password text NOT NULL
+CREATE TABLE Login(
+      username text PRIMARY KEY,
+      password text NOT NULL,
+      user_access text NOT NULL
 );
